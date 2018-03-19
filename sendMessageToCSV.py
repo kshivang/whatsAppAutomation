340661a38cfe24f9a3d3b7386ecd6af79e8ddfda
kshitij jaggi <kshitijjaggi@gmail.com>
2018-03-19 17:47:27 +0530

    counter for member fixed

--- sendMessageToCSV.py
+++ sendMessageToCSV.py
@@ -51,6 +51,7 @@ def read_csv(csv_to_open):
     for number in numbers:
         print(count, " out of ", len(numbers), "\n")
         send_message_to_number(number)
+	count = count + 1;
 
 
 def read_members_from_group():
